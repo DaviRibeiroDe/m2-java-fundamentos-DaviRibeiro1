183ce26feae6d39f1e65cb99ca695d8617e09bee
DaviRibeiro <davi_r_campos@estudante.sesisenai.org.br>
2026-03-11 11:01:46 -0300

    adicionado diretorio aulaFundamentos

---
+++ Aula01_Fundamentos/src/br/senai/ds2026/javafundamentos/aula01_console/Exemplo01CadastroFuncionario.java
@@ -0,0 +1,12 @@
+package br.senai.ds2026.javafundamentos.aula01_console;
+import java.util.Scanner;
+
+public class Exemplo01CadastroFuncionario {
+
+	public static void main(String[] args) {
+
+
+
+	}
+
+}
---
+++ Aula01_Fundamentos/src/br/senai/ds2026/javafundamentos/aula03_condicionais/Exemplo03AvaliacaoDesempenho.java
@@ -0,0 +1,13 @@
+package br.senai.ds2026.javafundamentos.aula03_condicionais;
+import java.util.Scanner;
+
+public class Exemplo03AvaliacaoDesempenho {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+
+	}
+
+}
